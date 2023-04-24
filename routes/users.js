f445d3872b630ce27dd968e8accdf287975f8487
Shevchenko Ekaterina <shev4enko.ekat@yandex.ru>
2023-04-24 21:13:24 +0300

    Б3

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,6 @@ const auth = require('../middlewares/auth');
 const regex = require('../utils/constants');
 
 usersRouter.get('/me', auth, getOwner);
-usersRouter.get('/:userId', auth, celebrate({
-  body: Joi.object().keys({
-    userId: Joi.string().hex().length(24).required(),
-  }),
-}), getUserById);
 usersRouter.patch('/me', auth, celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),

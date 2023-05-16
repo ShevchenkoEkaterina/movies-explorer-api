dd107f79158b1fed5d4379a1cbe0e5ea08a13261
Shevchenko Ekaterina <shev4enko.ekat@yandex.ru>
2023-05-16 20:24:47 +0300

    Корс

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const cors = require('cors');
 const limiter = require('./utils/limiter');
 const mongodb = require('./utils/mongodb');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const ALLOWED_CORS = [
+  'http://localhost:3000',
+  'https://api.movies.ekatshev4enko.nomoredomains.work',
+  'https://movies.ekatshev4enko.nomoredomains.work',
+];
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -23,6 +30,9 @@ app.use(express.json());
 
 app.use('/', router);
 
+app.use(cors({
+  origin: ALLOWED_CORS,
+}));
 app.use(errorLogger);
 app.use(errors());
 app.use(error);

2eda66ed131aedccf4ec603e579ea457737f995b
Shevchenko Ekaterina <shev4enko.ekat@yandex.ru>
2023-05-16 21:00:34 +0300

    Cors

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 require('dotenv').config({ path: './.env' });
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('cors');
 const limiter = require('./utils/limiter');
 const mongodb = require('./utils/mongodb');
 const router = require('./routes/index');
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const ALLOWED_CORS = [
+const corsLinks = [
   'http://localhost:3000',
   'https://api.movies.ekatshev4enko.nomoredomains.work',
   'https://movies.ekatshev4enko.nomoredomains.work',
@@ -21,6 +21,9 @@ const app = express();
 
 mongoose.connect(mongodb);
 mongoose.set('strictQuery', false);
+app.use(cors({
+  origin: corsLinks,
+}));
 
 app.use(requestLogger);
 
@@ -30,9 +33,6 @@ app.use(express.json());
 
 app.use('/', router);
 
-app.use(cors({
-  origin: ALLOWED_CORS,
-}));
 app.use(errorLogger);
 app.use(errors());
 app.use(error);
